bb34c5c3dca66216fb497c2981cbba78972181a1
Mia Rolfe <30848697+miarolfe@users.noreply.github.com>
2024-08-30 14:31:36 +0100

    Dockerize this

--- ducss_frontend/vite.config.ts
+++ ducss_frontend/vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	preview: {
+		host: true,
+		port: 80
+	},
 	plugins: [sveltekit()]
 });
