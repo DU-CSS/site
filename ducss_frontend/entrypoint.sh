77495d2ed3d1e39a7e969e2f4adfa1b2d6d829e2
Mia Rolfe <30848697+miarolfe@users.noreply.github.com>
2024-08-30 20:00:27 +0100

    The way I

--- ducss_frontend/entrypoint.sh
+++ ducss_frontend/entrypoint.sh
@@ -3,6 +3,9 @@
 # Start Nginx in the background
 nginx &
 
+# Wait for Nginx to start properly
+sleep 10
+
 # Run Certbot to obtain the certificates
 certbot --nginx -d ducss.ie -d www.ducss.ie --non-interactive --agree-tos --email ducss@csc.tcd.ie
 
